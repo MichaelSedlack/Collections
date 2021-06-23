3fd1397f7980ccb0be2e6e53e3be2febe408f52a
Ricardo Ruiz <kxngvenom@gmail.com>
2021-06-23 10:40:26 -0400

    [ADD] Boilerplate code to the server.js
    Description:
    - Installed middelware
    - Initialized app
    - Initialized middleware for the app (cors, body-parser)
    - Added environment variable needed for differentiating between production/dev environment

---
+++ server.js
@@ -0,0 +1,28 @@
+// IMPORTS
+require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+//Initializing Middleware
+app.use(cors()); // Allows for cross-origin requests
+app.use(bodyParser.json()); // Parses json in request.body for us
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('frontend/build')); // Serves frontend if in production mode
+}
+
+
+
+
+
+
+
+
+// Starts App
+app.listen(PORT, () => {
+  console.log('Server running on port ' + PORT);
+})
