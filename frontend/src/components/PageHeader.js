3b959b2517f5485cf6ab42e5823b187ef82013fc
Han Winchester <han_winchester@knights.ucf.edu>
2021-06-25 23:32:06 -0400

    added page header component

---
+++ frontend/src/components/PageHeader.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../App.css';
+
+function PageHeader(){
+    return(
+        <div>
+            <header>
+                <h1>Title</h1>
+            </header>
+            
+        </div>
+        
+    );
+};
+
+export default PageHeader;
