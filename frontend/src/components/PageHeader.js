bcf32cd1b4104c85f63d5c88db5d843886204b3c
Han Winchester <han_winchester@knights.ucf.edu>
2021-06-26 14:50:59 -0400

    added routing to header

--- frontend/src/components/PageHeader.js
+++ frontend/src/components/PageHeader.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import '../App.css';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 
 function PageHeader(){
     return(
+
         <div>
-            <header>
-                <h1>Title</h1>
-            </header>
+            <Router>
+                <Switch>
+                    <Route path='/' exact>
+                        <header>
+                            <h1>Collections</h1>
+                        </header>
+                    </Route>
+                    <Route path='/register' exact>
+                        <header>
+                            <h1>Create New Account</h1>
+                        </header>
+                    </Route>
+                </Switch>
+            </Router>
+            
             
         </div>
         
