35c79ca16aec8c80ba7a4e074dfaa5bc31eaf131
Han Winchester <han_winchester@knights.ucf.edu>
2021-06-25 23:31:45 -0400

    added page footer component

---
+++ frontend/src/components/PageFooter.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../App.css';
+
+function PageFooter(){
+    return(
+        <div>
+            <footer>
+                <p>Collections: Large Project</p>
+                <p>Group 6 Summer 2021</p>    
+            </footer>
+            
+        </div>
+        
+    );
+};
+
+export default PageFooter;
