fe07bb0508810c926d0c3ce52e8623d813f446dd
Ricardo Ruiz <ricardo@rruiz.dev>
2021-06-28 10:40:41 -0400

    More work on authorization using JWT
    [ADD] getToken() function which retrieves the authorization token from request headers.
    [ADD] Get User by ID endpoint and some validation:
            - Returns users whole profile if user accessing is self.
            - Returns only firstName and ID field if user accessing is not self.

--- utils/token.js
+++ utils/token.js
@@ -1,59 +1,59 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-function createToken( firstName, lastName, id )
-{
+function createToken( firstName, lastName, id ){
     return _createToken( firstName, lastName, id );
 }
 
-_createToken = function ( firstName, lastName, id )
-{
-    try
-    {
+function _createToken ( firstName, lastName, id ){
+    
+  try{
       const expiration = new Date();
       const user = {id,firstName,lastName};
 
-      //const accessToken =  jwt.sign( user, process.env.SECRET);
+      // const accessToken =  jwt.sign( user, process.env.SECRET);
 
       // In order to exoire with a value other than the default, use the 
        // following
       
-      const accessToken= jwt.sign(user,process.env.SECRET, 
-         { expiresIn: '30m'} );
-                       '24h'
-                      '365d'
+      const accessToken= jwt.sign(user,process.env.SECRET, { expiresIn: '30m'} );
+      //                  '24h'
+      //                 '365d'
       
 
       var ret = accessToken;
     }
-    catch(e)
-    {
+    catch(e){
       var ret = {error:e.message};
     }
+    
     return ret;
 }
 
-function isExpired( token )
-{
-   var isError = jwt.verify( token, process.env.SECRET, 
-     (err, verifiedJwt) =>
-   {
-     if( err )
-     {
-       return true;
-     }
-     else
-     {
-       return false;
-     }
-   });
+// GETS JWT TOKEN FROM HEADER
+function getToken(request){
+  const authorization = request.get('authorization');
 
-   return isError;
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+      return authorization.substring(7);
+  }
 
+  return null;
 }
 
-function refresh( token )
-{
+function isExpired( token ){
+  return jwt.verify( token, process.env.SECRET, (err, verifiedJwt) =>
+  {
+    if( err ){
+      return "Error";
+    }
+    else{
+      return verifiedJwt;
+    }
+  });
+}
+
+function refresh( token ){
   var ud = jwt.decode(token,{complete:true});
 
   var userId = ud.payload.id;
@@ -66,5 +66,6 @@ function refresh( token )
 module.exports = {
   refresh,
   isExpired,
-  createToken
+  createToken,
+  getToken
 }
