d51622b4170f6b6aa41fac23ecaf491cd428f552
Han Winchester <han_winchester@knights.ucf.edu>
2021-06-28 15:19:40 -0400

    added password check and bcrypt

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+//import bcrypt from 'bcrypt';
 
 function Register()
 {
@@ -8,9 +9,17 @@ function Register()
     var registerFirstName;
     var registerLastName;
     var registerEmail;
-    var registerPassword;
+    var registerPassword; // hash password????
 
     const [message,setMessage] = useState('');
+
+    // bcrypt.hash(registerPassword, 10, (err, hash) => {
+    //   if(err){
+    //     console.error(err);
+    //     return;
+    //   }
+    //   console.log(hash);
+    // })
 
     const doRegister = async event => 
     {
@@ -53,6 +62,17 @@ function Register()
         });
     };
 
+    const checkValidation = (e) => {
+      
+      if(registerPassword.value !== e.target.value){
+        setMessage("Passwords need to match");
+      }
+      else{
+          setMessage("The passwords match!");
+      }
+    }
+
+
     return(
       <div id="registerDiv">
         <span id="inner-title">PLEASE REGISTER</span><br />
@@ -60,10 +80,11 @@ function Register()
         <input type="text" id="registerLastName" placeholder="Last Name" ref={(c) => registerLastName = c}  /><br />
         <input type="text" id="registerEmail" placeholder="Email" ref={(c) => registerEmail = c}  /><br />
         <input type="password" id="registerPassword" placeholder="Password" ref={(c) => registerPassword = c} /><br />
+        <input type="password" id="registerConfirmPassword" placeholder="Confirm Password" onChange={(e)=> checkValidation(e)} /><br />
         <input type="submit" id="registerButton" className="buttons" value = "Complete Registration"
           onClick={doRegister} />
-        <span id="registerResult">{message}</span>
         <input type="submit" id="loginButton" className="buttons" value="Login" onClick={()=>{window.location.href = '/'}}/>
+        <span id="registerResult">{message}</span>
      </div>
     );
 };
