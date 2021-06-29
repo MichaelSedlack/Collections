a5eacc0659816c368746a0509c30e9157086b307
Han Winchester <han_winchester@knights.ucf.edu>
2021-06-28 21:34:45 -0400

    added jwt to login

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -36,29 +36,27 @@ function Login()
             var res = response.data;
             if (res.error) 
             {
-                setMessage(res.error);
+                setMessage("User/Password combination incorrect");
             }
             else 
             {	
-                //storage.storeToken(res);
-                //var jwt = require('jsonwebtoken');
+                storage.storeToken(res);
+                var jwt = require('jsonwebtoken');
     
-                //var ud = jwt.decode(storage.retrieveToken(),{complete:true});
-                //var userId = ud.payload.userId;
-                //var firstName = ud.payload.firstName;
-                //var lastName = ud.payload.lastName;
+                var ud = jwt.decode(storage.retrieveToken(),{complete:true});
+                var userId = ud.payload.userId;
+                var firstName = ud.payload.firstName;
+                var lastName = ud.payload.lastName;
                   
-                //var user = {firstName:firstName,lastName:lastName,id:userId}
-                //localStorage.setItem('user_data', JSON.stringify(user));
+                var user = {firstName:firstName,lastName:lastName,id:userId}
+                localStorage.setItem('user_data', JSON.stringify(user));
                 
-                //alert("ID: " + res.id + " \n" + "email: " + res.email);
                 setMessage("Logging In");
                 setTimeout(
                     function(){
                             
                             window.location.href = '/';
                     },2000)
-                //window.location.href = '/register';
             }
         })
         .catch(function (error) 
