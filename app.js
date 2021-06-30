909fe49d8fbfa049c7cdfbedd15d9e488823868f
Ricardo Ruiz <ricardo@rruiz.dev>
2021-06-30 12:25:54 -0400

    Refactoring and adding testing.

---
+++ app.js
@@ -0,0 +1,30 @@
+// IMPORTS
+const config = require('./utils/config');
+require('express-async-errors');
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const usersRouter = require('./controllers/users');
+const roomsRouter = require('./controllers/rooms');
+
+// Variables
+const app = express();
+const url = config.MONGODB_URI;
+
+// Database Connection
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => console.log("Mongo DB connected"))
+.catch(err => console.log(err));
+
+//Initializing Middleware
+app.use(cors()); // Allows for cross-origin requests
+app.use(express.json()); // Parses json in request.body for us
+app.use('/users', usersRouter); // Users route
+app.use('/rooms', roomsRouter); // Rooms route
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('frontend/build')); // Serves frontend if in production mode
+}
+
+module.exports = app;
---
+++ tests/user_api.test.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+const app = require('../app');
+const User = require('../models/user');
+
+const api = supertest(app);
+
+const initialUsers = [
+  {
+    firstName: 'Ricardo',
+    lastName: 'Ruiz',
+    email: 'kxngvenom@gmail.com',
+    passwordHash: 'testymctesterson'
+  }
+]
+
+beforeEach(async () => {
+  // Tear Down DB
+  await User.deleteMany({});
+
+  // Set users
+  const userObjects = initialUsers.map(user => new User(user));
+  const promiseArray = userObjects.map(user => user.save());
+  await Promise.all(promiseArray);
+})
+
+describe('/users/register', () => {
+
+  test('User creation successfull', async () => {
+    const newUser = {
+      firstName: 'Tester',
+      lastName: 'Testerson',
+      email: 'testy@tester.com',
+      passwordHash: 'test'
+    }
+
+  await api
+    .post('/users/register')
+    .send(newUser)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+  });
+
+})
+
+afterAll(() => {
+  mongoose.connection.close();
+})
