b8f9cd873bddfe09c34e446192e36abb1b37d240
Han Winchester <han_winchester@knights.ucf.edu>
2021-06-30 22:30:37 -0400

    made it so the create new room form only appears if user clicks a button

--- frontend/src/components/Museum.js
+++ frontend/src/components/Museum.js
@@ -3,7 +3,7 @@ import CreateRoomForm from './CreateRoomForm';
 
 function Museum() {
 
-    const [createRoomForm,setCreateRoomForm] = useState(<input type="submit" id="createRoomFormButton" className="buttons" value="Create New Room" onClick={() => createNewRoomForm()}/>);
+    const [createRoomForm,setCreateRoomForm] = useState(<div><input type="submit" id="createRoomFormButton" className="buttons" value="Create New Room" onClick={() => createNewRoomForm()}/><br /></div>);
 
     const createNewRoomForm = () => {
         setCreateRoomForm(<CreateRoomForm />)
@@ -15,8 +15,10 @@ function Museum() {
             {/*<input type="submit" id="createRoomFormButton" className="buttons" value="Create New Room" onClick={() => createNewRoomForm()}/>*/}
             <br />
             <span id="createNewRoomFormResult" >{createRoomForm}</span>
-            <input type="submit" id="loginButton" className="buttons" value="Login" onClick={()=>{window.location.href = '/'}}/>
-
+            
+            <input type="submit" id="cancelButton" className="buttons" value="Cancel" onClick={()=>{setCreateRoomForm(<input type="submit" id="createRoomFormButton" className="buttons" value="Create New Room" onClick={() => createNewRoomForm()}/>)}} />
+            <br />
+            <input type="submit" id="loginButton" className="buttons" value="Sign Out" onClick={()=>{window.location.href = '/'}}/> <br />
         </div>
         
     )
