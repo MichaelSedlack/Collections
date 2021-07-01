670a877752f6e81fd7583050ae406b680f3fcdf2
Han Winchester <han_winchester@knights.ucf.edu>
2021-06-30 23:25:51 -0400

    basic show/hide form and cancel button functions

--- frontend/src/components/Museum.js
+++ frontend/src/components/Museum.js
@@ -1,22 +1,30 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import CreateRoomForm from './CreateRoomForm';
 
 function Museum() {
 
     const [createRoomForm,setCreateRoomForm] = useState(<div><input type="submit" id="createRoomFormButton" className="buttons" value="Create New Room" onClick={() => createNewRoomForm()}/><br /></div>);
+    const [cancelButton, setCancelButton] = useState(false);
+
 
     const createNewRoomForm = () => {
+        setCancelButton(true)  
         setCreateRoomForm(<CreateRoomForm />)
+        
     };
+
+    function cancelClicked() {
+        setCancelButton(false)
+        {window.location.href = '/museum'}
+    };
+
 
 
     return (
         <div id="museumDiv">
-            {/*<input type="submit" id="createRoomFormButton" className="buttons" value="Create New Room" onClick={() => createNewRoomForm()}/>*/}
             <br />
             <span id="createNewRoomFormResult" >{createRoomForm}</span>
-            
-            <input type="submit" id="cancelButton" className="buttons" value="Cancel" onClick={()=>{setCreateRoomForm(<input type="submit" id="createRoomFormButton" className="buttons" value="Create New Room" onClick={() => createNewRoomForm()}/>)}} />
+            {cancelButton ? <input type="submit" id="cancelButton" className="buttons" value="Cancel" onClick={()=>{cancelClicked()}}/> : null}
             <br />
             <input type="submit" id="loginButton" className="buttons" value="Sign Out" onClick={()=>{window.location.href = '/'}}/> <br />
         </div>
