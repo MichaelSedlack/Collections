ca942586cc20498369313f71dd03fb9d86cacbb6
delrba <delrosario.brian.a@knights.ucf.edu>
2021-07-04 13:44:20 -0400

    added collection backend & item backend placeholder

---
+++ controllers/items.js
@@ -0,0 +1,18 @@
+// IMPORTS/DECLARATIONS
+const itemRouter = require('express').Router();
+const { Collection } = require('mongoose');
+const Item = require('../models/item');
+const Room = require('../models/room');
+const Collection = require('../models/collection');
+const User = require('../models/user');
+const token = require('../utils/token');
+
+// TODO: ROUTES
+// Create Item
+// GET Item
+// Update Item
+// Delete Item
+
+
+// EXPORTS
+module.exports = itemRouter;
