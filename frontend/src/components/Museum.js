abf8ea96bdd827e56b177fa5e4b99f4ad24b7bb9
Han Winchester <han_winchester@knights.ucf.edu>
2021-07-03 23:21:41 -0400

    added background color to grid items

--- frontend/src/components/Museum.js
+++ frontend/src/components/Museum.js
@@ -27,15 +27,15 @@ function Museum() {
             <div id="museumDiv">
                 <Grid container spacing={3}>
                     <Grid item xs={12}/>
-                    <Grid item xs={7} >
+                    <Grid style={{backgroundColor: '#9F2BC1'}} item xs={7} >
                         <TextField id="outlined-basic" label="Search Rooms" variant="outlined" />
                         <Button variant="contained" size="large" color="primary" type="submit" id="searchButton" className="buttons" value="Search" onClick={()=>{alert("search button clicked!")}}>Search Rooms</Button>
                     </Grid>
-                    <Grid item xs={5}>
+                    <Grid style={{backgroundColor: '#4C3AE3'}} item xs={5}>
                         <Button variant="contained" size="large" color="secondary" type="submit" id="loginButton" className="buttons" value="Sign Out" onClick={()=>{window.location.href = '/'}}>Sign Out</Button> <br />
                     </Grid>
                     <Grid item xs={12}/>
-                    <Grid item xs={7}>
+                    <Grid style={{backgroundColor: '#D31A50'}} item xs={7}>
                         <h1>Search Results Here</h1>
                         <h1>Search Results Here</h1>
                         <h1>Search Results Here</h1>
@@ -45,7 +45,7 @@ function Museum() {
                         <h1>Search Results Here</h1>
                         <h1>Search Results Here</h1>
                     </Grid>
-                    <Grid item xs={5} sm={5}>
+                    <Grid style={{backgroundColor: '#758283'}} item xs={5} sm={5}>
                         <span id="createNewRoomFormResult" >{createRoomForm}</span><br />
                         {cancelButton ? <Button variant="contained" size="large" color="secondary" type="submit" id="cancelButton" className="buttons" value="Cancel" onClick={()=>{cancelClicked()}}>Cancel</Button> : null}
                     </Grid>
