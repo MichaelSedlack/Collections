bcc60cfdfb3ef1869aa15c6b6f307d90dfbab516
Ricardo Ruiz <ricardo@rruiz.dev>
2021-07-06 12:39:06 -0400

    Fix

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use('/rooms', roomsRouter); // Rooms route
 
 if(process.env.NODE_ENV === 'production'){
   app.use(express.static(path.join(__dirname, "frontend", "build")));
-  app.get("*", (req, res) => {
+  app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
   });; // Serves frontend if in production mode
 }
