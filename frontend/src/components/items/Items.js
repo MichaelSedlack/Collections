24bb87cfc9f672c86b722db42eea1b5adbca0e7f
Han Winchester <han_winchester@knights.ucf.edu>
2021-07-22 15:26:25 -0400

    added navigation buttons

--- frontend/src/components/items/Items.js
+++ frontend/src/components/items/Items.js
@@ -1,61 +1,60 @@
-import React, { useEffect, useState, useContext } from 'react';
-import { useHistory } from 'react-router-dom';
-import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
-import CreateItemForm from './CreateItemForm';
-import ItemForm from './ItemForm';
-import SearchItems from './SearchItems';
-import itemService from '../helpers/itemService';
-import { UserContext, CollectionContext } from './../UserContext';
-import { ApiContext } from '../ApiContext';
+import React, { useEffect, useState, useContext } from "react";
+import { useHistory } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import CreateItemForm from "./CreateItemForm";
+import ItemForm from "./ItemForm";
+import SearchItems from "./SearchItems";
+import itemService from "../helpers/itemService";
+import { UserContext, RoomContext, CollectionContext } from "./../UserContext";
+import { ApiContext } from "../ApiContext";
 
 function Items() {
-  const {user, setUser} = useContext(UserContext)
-  const {collection} = useContext(CollectionContext);
+  const { user } = useContext(UserContext);
+  const { room } = useContext(RoomContext);
+  const { collection } = useContext(CollectionContext);
 
   const history = useHistory();
 
-
   // Initial States
-  const [message,setMessage] = useState('');
+  const [message, setMessage] = useState("");
   const [error, setError] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
   const [items, setItems] = useState([]);
   const [keys, setKeys] = useState([]);
 
-
   // only fires at beginning
-    useEffect(() => {
+  useEffect(() => {
     (async () => {
-      try{
+      try {
         setIsLoading(true);
 
         const res = await itemService.getAll(collection.id);
 
-        if(res.error){
+        if (res.error) {
           setIsLoading(false);
           setError(true);
           setMessage(res.error);
           return;
         }
-        console.log(`Response from API ITEMS: ${res.keys}`)
+        console.log(`Response from API ITEMS: ${res.keys}`);
         setKeys(res.keys);
         setItems(res.items);
         setIsLoading(false);
-      }catch(exception){
+      } catch (exception) {
         setIsLoading(false);
         setError(true);
         setMessage(exception.message);
         console.log(exception);
       }
-    })()
-  },[user])
-    
+    })();
+  }, [user]);
+
   const doCreate = async (item) => {
-    try{
+    try {
       const res = await itemService.create(item);
 
-      if(res.error){
+      if (res.error) {
         setIsLoading(false);
         setError(true);
         setMessage(res.error);
@@ -64,19 +63,19 @@ function Items() {
       setIsLoading(false);
       const newItems = [...items, res];
       setItems(newItems);
-    }catch(exception){
+    } catch (exception) {
       setIsLoading(false);
       setError(true);
       setMessage(exception.message);
       console.log(exception);
     }
-  }
+  };
 
   const doSearch = async (search) => {
-    try{
+    try {
       const res = await itemService.search(search, collection.id);
 
-      if(res.error){
+      if (res.error) {
         setIsLoading(false);
         setError(true);
         setMessage(res.error);
@@ -84,116 +83,136 @@ function Items() {
       }
       setIsLoading(false);
       setItems(res);
-    }catch(exception){
+    } catch (exception) {
       setIsLoading(false);
       setError(true);
       setMessage(exception.message);
       console.log(exception);
     }
-  }
-
+  };
 
   const doDelete = async (itemID) => {
-    try{
+    try {
       const res = await itemService.deleteItem(itemID);
 
-      if(res.error){
+      if (res.error) {
         setIsLoading(false);
         setError(true);
         setMessage(res.error);
         return res;
       }
       setIsLoading(false);
-      setTimeout(function(){
-        setItems(items.filter(item => item.id !== itemID));
+      setTimeout(function () {
+        setItems(items.filter((item) => item.id !== itemID));
         return res;
-      },1000)
+      }, 1000);
 
       return true;
-    }catch(exception){
+    } catch (exception) {
       setIsLoading(false);
       setError(true);
       setMessage(exception.message);
       console.log(exception);
     }
-  }
+  };
 
-  const doUpdate = async ( itemID, newItem ) => {
-    try{
+  const doUpdate = async (itemID, newItem) => {
+    try {
       const res = await itemService.update(itemID, newItem);
 
-      if(res.error){
+      if (res.error) {
         setIsLoading(false);
         setError(true);
         setMessage(res.error);
         return res;
       }
       setIsLoading(false);
-      setTimeout(function(){
-        setItems(items.map(item => {
-          if(itemID === item.id){
-            return {...item, ...newItem}
-          }
+      setTimeout(function () {
+        setItems(
+          items.map((item) => {
+            if (itemID === item.id) {
+              return { ...item, ...newItem };
+            }
 
-          return item;
-        }))
+            return item;
+          })
+        );
       }, 500);
-    }catch(exception){
+    } catch (exception) {
       setIsLoading(false);
       setError(true);
       setMessage(exception.message);
       console.log(exception);
     }
+  };
+
+  // If UseEffect hasn't finished then show loading message
+  if (isLoading) {
+    return <h4>Loading Webpage</h4>;
+  } else if (error) {
+    return <h1>{message}</h1>;
+  } else {
+    return (
+      <div>
+        <ApiContext.Provider value={{ doUpdate, doDelete, doSearch, doCreate }}>
+          <Grid container spacing={3}>
+            {/* Set up in to rows of length 12 */}
+            <Grid item xs={12} />
+
+            {/* Begin Row (This row is split into 2+5+5=12)*/}
+            <Grid item xs={2} />
+            <Grid item xs={5}>
+              <SearchItems />
+            </Grid>
+            <Grid item xs={5}>
+              <Button
+                onClick={() => {
+                  history.push("/museum");
+                }}
+                color="primary"
+              >
+                {room.name} Room
+              </Button>
+              {">>"}
+              <Button
+                onClick={() => {
+                  history.push("/collections");
+                }}
+                color="primary"
+              >
+                {collection.name} Collection
+              </Button>
+              {">>"}
+              <Button color="primary" color="secondary">
+                Items
+              </Button>
+            </Grid>
+            {/* End Row */}
+
+            {/* single row of nothing. Im using this to space things out */}
+            <Grid item xs={12} />
+
+            {/* Begin Row */}
+            <Grid item xs={1} />
+            <Grid item xs={4}>
+              <span id="displayItem">
+                <h1>{collection.name} Items</h1>
+              </span>
+              <ItemForm items={items} />
+              {error && <div>{message}</div>}
+            </Grid>
+            <Grid item xs={2} />
+            <Grid item xs={5}>
+              <CreateItemForm keys={keys} />
+            </Grid>
+            {/* End Row */}
+
+            <Grid item xs={12} />
+          </Grid>
+        </ApiContext.Provider>
+      </div>
+    );
   }
-
-
-    // If UseEffect hasn't finished then show loading message
-    if(isLoading){
-        return(
-            <h4>Loading Webpage</h4>
-        );
-    }else if(error){
-      return(<h1>{message}</h1>);
-    }else{
-        return(
-            <div>
-                <ApiContext.Provider value={{doUpdate, doDelete, doSearch, doCreate}}>
-                    <Grid container spacing={3}>
-                        {/* Set up in to rows of length 12 */}
-                        <Grid item xs={12}/>
-
-                        {/* Begin Row (This row is split into 2+5+5=12)*/}
-                        <Grid item xs={2}/>
-                        <Grid item xs={5}>    
-                            <SearchItems/>                    
-                        </Grid>
-                        <Grid item xs={5}>
-                            <Button variant="contained" size="large" color="primary" type="submit" id="roomButton" className="buttons" value="Back to Collections" onClick={()=>{history.push("/collections")}}>Back to Collections</Button> <br />
-                        </Grid>
-                        {/* End Row */}
-
-                        {/* single row of nothing. Im using this to space things out */}
-                        <Grid item xs={12}/>
-
-                        {/* Begin Row */}
-                        <Grid item xs={1}/>
-                        <Grid item xs={4}>
-                           <span id="displayItem"><h1>{collection.name} Items</h1></span>  
-                            <ItemForm items={items}/>
-                            {error && <div>{message}</div>}
-                        </Grid>
-                        <Grid item xs={2}/>
-                        <Grid item xs={5}>
-                            <CreateItemForm keys={keys}/>
-                        </Grid>
-                        {/* End Row */}
-
-                        <Grid item xs={12}/>
-                    </Grid>
-                </ApiContext.Provider>
-            </div>
-        )
-    }
 }
 
 export default Items;
--- frontend/src/components/rooms/Museum.js
+++ frontend/src/components/rooms/Museum.js
@@ -1,22 +1,21 @@
-import React, { useEffect, useState, useContext } from 'react';
-import { useParams } from 'react-router-dom';
-import CreateRoomForm from './CreateRoomForm';
-import Grid from '@material-ui/core/Grid';
-import RoomForm from './RoomForm';
-import SearchRooms from './SearchRooms';
-import { UserContext } from '../UserContext';
-import { ApiContext } from '../ApiContext';
-import roomService from '../helpers/roomService';
-
+import React, { useEffect, useState, useContext } from "react";
+import { useParams } from "react-router-dom";
+import CreateRoomForm from "./CreateRoomForm";
+import Grid from "@material-ui/core/Grid";
+import { Button } from "@material-ui/core";
+import RoomForm from "./RoomForm";
+import SearchRooms from "./SearchRooms";
+import { UserContext } from "../UserContext";
+import { ApiContext } from "../ApiContext";
+import roomService from "../helpers/roomService";
 
 function Museum() {
-
-  const {user, setUser} = useContext(UserContext)
+  const { user } = useContext(UserContext);
   const { userID } = useParams();
 
   // Initial States
   const [museumUser, setMuseumUser] = useState(null);
-  const [message,setMessage] = useState('');
+  const [message, setMessage] = useState("");
   const [error, setError] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
   const [rooms, setRooms] = useState([]);
@@ -24,15 +23,15 @@ function Museum() {
   // GETS INITIAL DATA
   useEffect(() => {
     (async () => {
-      try{
+      try {
         setIsLoading(true);
 
-        const id = (!userID) ? user.id : userID;
+        const id = !userID ? user.id : userID;
 
         const res = await roomService.getAll(id);
         const currUser = await roomService.getUser(id);
 
-        if(res.error){
+        if (res.error) {
           setIsLoading(false);
           setError(true);
           setMessage(res.error);
@@ -42,20 +41,20 @@ function Museum() {
         setRooms(res);
         setMuseumUser(currUser);
         setIsLoading(false);
-      }catch(exception){
+      } catch (exception) {
         setIsLoading(false);
         setError(true);
         setMessage(exception.message);
         console.log(exception);
       }
-    })()
-  },[userID, user])
+    })();
+  }, [userID, user]);
 
   const doCreate = async (room) => {
-    try{
+    try {
       const res = await roomService.create(room);
 
-      if(res.error){
+      if (res.error) {
         setIsLoading(false);
         setError(true);
         setMessage(res.error);
@@ -64,19 +63,19 @@ function Museum() {
       setIsLoading(false);
       const newRooms = [...rooms, res];
       setRooms(newRooms);
-    }catch(exception){
+    } catch (exception) {
       setIsLoading(false);
       setError(true);
       setMessage(exception.message);
       console.log(exception);
     }
-  }
+  };
 
   const doSearch = async (search) => {
-    try{
+    try {
       const res = await roomService.search(search, user.id);
 
-      if(res.error){
+      if (res.error) {
         setIsLoading(false);
         setError(true);
         setMessage(res.error);
@@ -84,111 +83,115 @@ function Museum() {
       }
       setIsLoading(false);
       setRooms(res);
-    }catch(exception){
+    } catch (exception) {
       setIsLoading(false);
       setError(true);
       setMessage(exception.message);
       console.log(exception);
     }
-  }
+  };
 
   const doDelete = async (roomID) => {
-    try{
+    try {
       const res = await roomService.deleteRoom(roomID);
 
-      if(res.error){
+      if (res.error) {
         setIsLoading(false);
         setError(true);
         setMessage(res.error);
         return res;
       }
       setIsLoading(false);
-      setTimeout(function(){
-        setRooms(rooms.filter(room => room.id !== roomID));
+      setTimeout(function () {
+        setRooms(rooms.filter((room) => room.id !== roomID));
         return res;
-      },1000)
+      }, 1000);
 
       return true;
-    }catch(exception){
+    } catch (exception) {
       setIsLoading(false);
       setError(true);
       setMessage(exception.message);
       console.log(exception);
     }
-  }
+  };
 
-  const doUpdate = async ( roomID, newRoom ) => {
-    try{
+  const doUpdate = async (roomID, newRoom) => {
+    try {
       const res = await roomService.update(roomID, newRoom);
 
-      if(res.error){
+      if (res.error) {
         setIsLoading(false);
         setError(true);
         setMessage(res.error);
         return res;
       }
       setIsLoading(false);
-      setTimeout(function(){
-        setRooms(rooms.map(room => {
-          if(roomID === room.id){
-            return {...room, ...newRoom}
-          }
+      setTimeout(function () {
+        setRooms(
+          rooms.map((room) => {
+            if (roomID === room.id) {
+              return { ...room, ...newRoom };
+            }
 
-          return room;
-        }))
+            return room;
+          })
+        );
       }, 500);
-    }catch(exception){
+    } catch (exception) {
       setIsLoading(false);
       setError(true);
       setMessage(exception.message);
       console.log(exception);
     }
-  }
+  };
 
-
-  if(isLoading){
-    return(<h4>Loading Webpage</h4>);
-  }
-  else if(error){
-    return(<h1>{message}</h1>);
-  }
-  else{
-    return(
+  if (isLoading) {
+    return <h4>Loading Webpage</h4>;
+  } else if (error) {
+    return <h1>{message}</h1>;
+  } else {
+    return (
       <div>
-        <ApiContext.Provider value={{doUpdate, doDelete, doSearch, doCreate}}>
+        <ApiContext.Provider value={{ doUpdate, doDelete, doSearch, doCreate }}>
           <Grid container spacing={3}>
             {/* Set up in to rows of length 12 */}
-            <Grid item xs={12}/>
+            <Grid item xs={12} />
 
             {/* Begin Row (This row is split into 2+5+5=12)*/}
-            <Grid item xs={2}/>
-            <Grid item xs={5}>    
-                <SearchRooms/>                    
+            <Grid item xs={2} />
+            <Grid item xs={5}>
+              <SearchRooms />
             </Grid>
-            <Grid item xs={5}/>
+            <Grid item xs={5}>
+              <Button color="secondary">Rooms</Button>
+            </Grid>
             {/* End Row */}
 
             {/* single row of nothing. Im using this to space things out */}
-            <Grid item xs={12}/>
+            <Grid item xs={12} />
 
             {/* Begin Row  DISPLAYS ROOMS*/}
-            <Grid item xs={1}/>
+            <Grid item xs={1} />
             <Grid item xs={4}>
-                <span id="displayRoom"><h1>{museumUser.firstName}'s Rooms</h1></span>
-                {error && <div>{message}</div>}
-                <RoomForm rooms={rooms}/>
+              <span id="displayRoom">
+                <h1>{museumUser.firstName}'s Rooms</h1>
+              </span>
+              {error && <div>{message}</div>}
+              <RoomForm rooms={rooms} />
             </Grid>
-            <Grid item xs={2}/>
+            <Grid item xs={2} />
+
             <Grid item xs={5}>
-                <CreateRoomForm/>
+              <CreateRoomForm />
             </Grid>
             {/* End Row */}
 
-            <Grid item xs={12}/>
+            <Grid item xs={12} />
           </Grid>
         </ApiContext.Provider>
       </div>
-    )
+    );
   }
 }
 
