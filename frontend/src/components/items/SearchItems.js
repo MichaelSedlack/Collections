d63b187f23e9a86169f9a74cf44a7f0ca7762238
Han Winchester <han_winchester@knights.ucf.edu>
2021-07-23 20:46:19 -0400

    updated text field text

--- frontend/src/components/items/SearchItems.js
+++ frontend/src/components/items/SearchItems.js
@@ -1,20 +1,26 @@
-import React, { useState, useContext } from 'react';
-import TextField from '@material-ui/core/TextField';
-import { ApiContext } from '../ApiContext';
+import React, { useState, useContext } from "react";
+import TextField from "@material-ui/core/TextField";
+import { ApiContext } from "../ApiContext";
 
-function SearchItems(){
+function SearchItems() {
   const { doSearch } = useContext(ApiContext);
-  const [search, setSearch] = useState('');
-    
-  const searchItem = async e => {
+  const [search, setSearch] = useState("");
+
+  const searchItem = async (e) => {
     setSearch(e.target.value);
     doSearch(e.target.value);
-  }
+  };
 
-  return(
-      <div>
-          <TextField id="outlined-basic" label="Search Collections" variant="outlined" value={search} onChange={(e) => searchItem(e)} />
-      </div>
+  return (
+    <div>
+      <TextField
+        id="outlined-basic"
+        label="Search Items"
+        variant="outlined"
+        value={search}
+        onChange={(e) => searchItem(e)}
+      />
+    </div>
   );
 }
 
