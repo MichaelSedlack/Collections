a5b529e9a65c68455c3bd867f3c37c942846d8e7
Han Winchester <han_winchester@knights.ucf.edu>
2021-07-23 20:23:21 -0400

    added window scaling

--- frontend/src/components/rooms/Museum.js
+++ frontend/src/components/rooms/Museum.js
@@ -8,6 +8,7 @@ import SearchRooms from "./SearchRooms";
 import { UserContext } from "../UserContext";
 import { ApiContext } from "../ApiContext";
 import roomService from "../helpers/roomService";
+import { breakpoints } from "@material-ui/system";
 
 function Museum() {
   const { user } = useContext(UserContext);
@@ -156,38 +157,39 @@ function Museum() {
         <ApiContext.Provider value={{ doUpdate, doDelete, doSearch, doCreate }}>
           <Grid container spacing={3}>
             {/* Set up in to rows of length 12 */}
-            <Grid item xs={12} />
+            <Grid item xs={12} md={12} lg={12} />
 
             {/* Begin Row (This row is split into 2+5+5=12)*/}
-            <Grid item xs={2} />
-            <Grid item xs={5}>
+            <Grid item xs={4} sm={2} md={2} lg={2} />
+            <Grid item xs={7} sm={5} md={5} lg={5}>
               <SearchRooms />
             </Grid>
-            <Grid item xs={5}>
+
+            <Grid item xs={12} sm={12} md={5} lg={5}>
               <Button color="secondary">Rooms</Button>
             </Grid>
             {/* End Row */}
 
             {/* single row of nothing. Im using this to space things out */}
-            <Grid item xs={12} />
+            <Grid item xs={12} md={12} lg={12} />
 
             {/* Begin Row  DISPLAYS ROOMS*/}
-            <Grid item xs={1} />
-            <Grid item xs={4}>
+            <Grid item xs={1} md={1} lg={1} />
+            <Grid item xs={10} sm={9} md={5} lg={4}>
               <span id="displayRoom">
                 <h1>{museumUser.firstName}'s Rooms</h1>
               </span>
               {error && <div>{message}</div>}
               <RoomForm rooms={rooms} />
             </Grid>
-            <Grid item xs={2} />
+            <Grid item xs={2} sm={3} md={1} lg={2} />
 
-            <Grid item xs={5}>
+            <Grid item xs={10} sm={9} md={4} lg={5}>
               <CreateRoomForm />
             </Grid>
             {/* End Row */}
 
-            <Grid item xs={12} />
+            <Grid item xs={12} md={12} lg={12} />
           </Grid>
         </ApiContext.Provider>
       </div>
